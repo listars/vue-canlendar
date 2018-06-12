23aac3eac0a806eb3c0e75e16d93fefb5f0f4e02
listars <1048782867@qq.com>
2018-06-13 02:32:46 +0800

    create project

---
+++ src/vue-canlendar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vue-canlendar">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueCanlendar',
+  props: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+.vue-canlendar {
+
+}
+</style>
